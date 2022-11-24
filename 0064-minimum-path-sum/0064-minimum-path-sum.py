1775a1558eac969ca0774e1c2fd2b1bac7a9d07e
Piyush Lund <55160067+LundPiyush@users.noreply.github.com>
2022-11-25 03:23:05 +0530

    Time: 182 ms (69.78%), Space: 15.7 MB (84.34%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -15,4 +15,17 @@ class Solution:
         m = len(grid)
         n = len(grid[0])
         dp=[[-1]*n for _ in range(m)]
-        return self.f(m-1,n-1,grid,dp)
+        
+        for i in range(0,m):
+            for j in range(0,n):
+                if i== 0 and j ==0:
+                    dp[i][j] = grid[i][j]
+                else:
+                    up,left = float('inf'),float('inf')
+                    if i>0:
+                        up= grid[i][j] + dp[i-1][j]
+                    if j>0:
+                        left= grid[i][j] + dp[i][j-1]
+                    dp[i][j] = min(up,left)
+        return dp[m-1][n-1]
+        #return self.f(m-1,n-1,grid,dp) -> recursion
