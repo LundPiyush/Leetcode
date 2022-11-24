1f064b58ef04264b1253c574f36fa5682f1a7bde
Piyush Lund <55160067+LundPiyush@users.noreply.github.com>
2022-11-25 03:48:49 +0530

    Time: 46 ms (75.23%), Space: 14.1 MB (19.83%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -12,5 +12,17 @@ class Solution:
         return dp[ind]
     def rob(self, nums: List[int]) -> int:
         n=len(nums)
-        dp=[-1]*n
-        return self.f(n-1,nums,dp)
+        dp=[0]*n
+        #tabulation : 
+        
+        for i in range(0,n):
+            if i==0:
+                dp[i] = nums[0]
+            pick = nums[i]
+            if i>1:
+                pick += dp[i-2] # pick element and move back by 2
+            not_pick = 0 + dp[i-1] # not picked so don't add anything and move back by 1
+            
+            dp[i]=max(pick,not_pick)
+        return dp[n-1]
+        #return self.f(n-1,nums,dp) => recursion solution with memoziation
