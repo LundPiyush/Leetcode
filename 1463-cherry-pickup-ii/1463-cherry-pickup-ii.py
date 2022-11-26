86fab3f341f189c78ef58ce2d3ff479d4c329f96
Piyush Lund <55160067+LundPiyush@users.noreply.github.com>
2022-11-27 05:27:52 +0530

    Time: 6569 ms (14.93%), Space: 21.9 MB (67.95%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.py
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.py
@@ -14,7 +14,9 @@ class Solution:
         if dp[i][j1][j2]!=-1:
             return dp[i][j1][j2]
         maxi = 0 
-        for d1 in range(-1,2,1):
+        
+        # explore all paths for alice and bob SIMULTANEOUSLY
+        for d1 in range(-1,2,1): # d1 goes from  -1 -> 1
             for d2 in range(-1,2,1):
                 if j1 == j2:
                     maxi = max(maxi,a[i][j1] + self.f(i+1,j1+d1,j2+d2,n,m,a,dp))
@@ -28,4 +30,48 @@ class Solution:
         n = len(grid)
         m = len(grid[0])
         dp=[[[-1]*m for _ in range(m)]for _ in range(n)]
-        return self.f(0,0,m-1,n,m,grid,dp)
+        
+        '''
+        Tabulation
+        1. Base case : a. reaching destination => n -1  
+        2. Express i,j1,j2 in terms of for loops and copy recurence
+        3. Return max
+        '''
+        
+        # Base case 
+        
+        for j1 in range(0,m):
+            for j2 in range(0,m):
+                if j1==j2:
+                    dp[n-1][j1][j2] = grid[n-1][j1]
+                else:
+                    dp[n-1][j1][j2] = grid[n-1][j1] + grid[n-1][j2]
+                
+        # explore all paths for alice and bob SIMULTANEOUSLY
+
+        for i in range(n-2,-1,-1):
+            for j1 in range(0,m):
+                for j2 in range(0,m):
+                    maxi=float('-inf')
+                    for d1 in range(-1,2,1): # d1 goes from  -1 -> 1
+                        for d2 in range(-1,2,1):
+                            value = 0
+                            if j1 == j2:
+                                if j1+d1>=0 and j2+d2>=0 and j1+d1<m and j2+d2<m:
+                                    value = max(value,grid[i][j1] + dp[i+1][j1+d1][j2+d2])      
+                                else:
+                                    value+=float('-inf')
+                            else:
+                                if j1+d1>=0 and j2+d2>=0 and j1+d1<m and j2+d2<m:
+                                    value = max(value,grid[i][j1] + grid[i][j2] + dp[i+1][j1+d1][j2+d2])
+                                else:
+                                    value+=float('-inf')
+                            maxi = max(maxi,value)
+                    dp[i][j1][j2] = maxi
+        return dp[0][0][m-1]
+        
+        
+        
+        
+        
+        #return self.f(0,0,m-1,n,m,grid,dp) => recursion 
