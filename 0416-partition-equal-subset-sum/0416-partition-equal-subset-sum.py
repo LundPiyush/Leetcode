d298c13f28f2b8ca24edbcc83f1a8cc07ee31d2f
Piyush Lund <55160067+LundPiyush@users.noreply.github.com>
2022-11-29 03:26:35 +0530

    Time: 7658 ms (9.78%), Space: 14.1 MB (80.00%) - LeetHub

---
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -0,0 +1,26 @@
+class Solution:
+    def subset(self,n,k,arr):
+        prev =[False]*(k+1)
+        cur =[False]*(k+1)
+        prev[0]=cur[0] = True
+        if arr[0]<=k:
+            prev[arr[0]] = True
+        for ind in range(1,n):
+            for target in range(k,1,-1):
+                not_take = prev[target]
+                take = False
+                if arr[ind]<= target:
+                    take = prev[target-arr[ind]]
+                cur[target] = take or not_take
+            cur = prev
+        return prev[k] 
+        
+    def canPartition(self, nums: List[int]) -> bool:
+        total_sum = 0
+        n = len(nums)
+        for i in range(len(nums)):
+            total_sum += nums[i]
+        if total_sum % 2 ==1:
+            return False
+        target = total_sum // 2
+        return self.subset(n,target,nums)
