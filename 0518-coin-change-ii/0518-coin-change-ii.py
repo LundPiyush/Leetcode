21c51a9a2fb9c6d5c85618ae36ebd92169c128fb
Piyush Lund <55160067+LundPiyush@users.noreply.github.com>
2022-12-14 21:09:51 +0530

    Time: 433 ms (68.58%), Space: 34.9 MB (27.22%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -17,5 +17,18 @@ class Solution:
     
     def change(self, amount: int, coins: List[int]) -> int:
         n = len(coins)
-        dp = [[-1]*(amount+1) for _ in range(n)]
-        return self.f(n-1,amount,coins,dp)
+        dp = [[0]*(amount+1) for _ in range(n)]
+        #return self.f(n-1,amount,coins,dp) => recursion
+        
+        for T in range(0,amount+1):
+            if T % coins[0]==0:
+                dp[0][T] = 1
+    
+        for ind in range(1,n):
+            for T in range(0,amount+1):
+                not_take = dp[ind-1][T]
+                take = 0 
+                if coins[ind]<=T:
+                    take = dp[ind][T-coins[ind]]
+                dp[ind][T] = not_take + take
+        return dp[n-1][amount]
