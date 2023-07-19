38ba0f0a7ea9c20e114550b788988c84db613695
Piyush Lund <55160067+LundPiyush@users.noreply.github.com>
2023-07-19 23:29:09 +0530

    Time: 363 ms (26.82%), Space: 16.6 MB (74.66%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.py
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.py
@@ -5,7 +5,8 @@ class Solution:
         for ele in nums:
             if ele == 1:
                 count+=1
+                max_count= max(max_count,count)
             else:
                 count = 0
-            max_count= max(max_count,count)
+            
         return max_count
