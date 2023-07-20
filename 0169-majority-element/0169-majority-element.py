720c6845ebf909413a3aea27c265aa19ea460648
Piyush Lund <55160067+LundPiyush@users.noreply.github.com>
2023-07-21 02:16:04 +0530

    Time: 181 ms (36.95%), Space: 17.8 MB (72.93%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.py
@@ -0,0 +1,45 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        '''
+        storing ele,count in hash_map and iterating over hash_map
+        TC => O(Nlogn) + O(n) 
+        SC =>O(x) 
+        x = unique elements 
+        
+        hash_map={}
+        for ele in nums:
+            if ele not in hash_map:
+                hash_map[ele]=1
+            else:
+                hash_map[ele]= hash_map[ele] + 1
+        n = len(nums)
+        for key,element in hash_map.items():
+            if element > n // 2:
+                return key
+        '''
+        
+        '''
+        
+        Moore Voting algorithm
+        TC=> O(n) + O(n)
+        SC => O(1)
+        '''
+        
+        cnt = 0
+        ele = 0
+        n= len(nums)
+        for i in range(n):
+            if cnt == 0:
+                ele = nums[i]
+                cnt+=1
+            elif ele == nums[i]:
+                cnt+=1
+            else:
+                cnt-=1
+        cnt2=0
+        for i in range(n):
+            if ele == nums[i]:
+                cnt2+=1
+        if cnt2 >n//2:
+            return ele
+        
