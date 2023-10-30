8a14e05c186eed39a80b53f843ad5aebfdc18e99
Piyush Lund <55160067+LundPiyush@users.noreply.github.com>
2023-10-30 22:43:30 +0530

    Time: 120 ms (36.39%), Space: 17.1 MB (63.79%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -1,8 +1,48 @@
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
-        """
-        Do not return anything, modify matrix in-place instead.
-        """
+        n = len(matrix)   
+        m= len(matrix[0])  
+        col0 = 1
+        #row=n*[0]   #row-array ->  matrix[...][0]
+        #col=m*[0]   #column-array -> matrix[0][...]
+        
+        # step 1: Traverse the matrix and
+        # mark 1st row & col accordingly
+        for i in range(n):
+            for j in range(m):
+                if matrix[i][j] == 0:
+                    matrix[i][0] = 0
+                    if j!= 0:
+                        matrix[0][j] = 0
+                    else:
+                        col0 = 0
+        
+        # Step 2: Mark with 0 from (1,1) to (n-1, m-1):
+        for i in range(1,n):
+            for j in range(1,m):
+                if matrix[i][j]!=0:
+                    if matrix[i][0] == 0 or matrix[0][j] == 0:
+                        matrix[i][j] = 0
+                        
+        
+        #step 3: Finally mark the 1st col & then 1st row:
+        if matrix[0][0] == 0:
+            for j in range(m):
+                matrix[0][j] = 0
+        
+        if col0 == 0:
+            for i in range(n):
+                matrix[i][0] = 0
+        
+    """
+    Better approach :
+    
+    Time Complexity: O(2*(N*M)), where N = no. of rows, M = no. of columns.
+    Reason: We are traversing the entire matrix 2 times and each traversal is taking O(N*M) time complexity.
+    
+    Space Complexity: O(N) + O(M) 
+    Reason: O(N) is for using the row array and O(M) is for using the col array.
+
         n = len(matrix)   
         m= len(matrix[0])  
         
@@ -20,6 +60,29 @@ class Solution:
                 if row[i] or col[j]:
                     matrix[i][j] = 0
                     
-        print(matrix)
-                    
+        """
         
+            
+        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
