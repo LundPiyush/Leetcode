0f1b89daeab37ffafbca9e9ae72d5a0d7b699e6a
Piyush Lund <55160067+LundPiyush@users.noreply.github.com>
2023-11-02 01:34:08 +0530

    Time: 39 ms (94.19%), Space: 16.1 MB (79.11%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -5,17 +5,19 @@ class Solution:
         """
         n  = len(nums)
         ind=-1
+        # Step 1: Find the break point:
         for i in range(n-2,-1,-1):
             if nums[i] < nums[i+1]:
                 ind = i
                 break
-        print("ind",ind)
-        if ind == -1:
+        if ind == -1:  # If break point does not exist:
             nums.reverse()
         else:
+            # Step 2: Find the next greater element and swap it with arr[ind]:
             for i in range(n-1,ind,-1):
                 if nums[i] > nums[ind]:
                     nums[i],nums[ind] = nums[ind],nums[i]
                     break
+            # Step 3: reverse the right half:        
             nums[ind+1:] = reversed(nums[ind+1:])
             
