79993b3c6d37c8f40390ba23b2c26602d0c4fb93
Piyush Lund <55160067+LundPiyush@users.noreply.github.com>
2023-11-06 23:56:46 +0530

    Time: 44 ms (52.27%), Space: 16.3 MB (26.03%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,15 +3,20 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        i = m-1
-        j = n-1
-        k = m + n - 1
+        # USING 3 pointers 
+        i = m-1 # pointing towards last element of arr1
+        j = n-1  # pointing towards last element of arr2
+        k = m + n - 1  # pointing towards last 0 present in arr1
+        
+        #looping as long as we have elements in arr2
         while j >= 0:
-            if i >= 0 and nums1[i]>nums2[j]:
+            # if arr1 has elements and arr1[i]> arr2[j] then pick that ele and put it at kth position
+            if i >= 0 and nums1[i] > nums2[j]:
                 nums1[k]= nums1[i]
                 k=k-1
                 i=i-1
             else:
+                # if arr1[i] <  arr2[j] then pick ele from arr2 and put it at kth position
                 nums1[k] = nums2[j]
                 k=k-1
                 j=j-1
