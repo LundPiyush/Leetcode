d5954f4e613ea0042955c43d627d2191ae4b7c2e
Piyush Lund <55160067+LundPiyush@users.noreply.github.com>
2023-11-07 22:52:26 +0530

    Time: 535 ms (51.50%), Space: 39.3 MB (5.36%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -1,6 +1,21 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
+        frequency_hash={}
+        for i in range(len(nums)):
+            if nums[i] in frequency_hash:
+                frequency_hash[nums[i]] = frequency_hash[nums[i]] + 1
+            else:
+                frequency_hash[nums[i]] = 1
+          
+        for k in frequency_hash.keys():
+            if frequency_hash[k]>1:
+                return k
+            
+    '''
         nums.sort()
         for i in range(len(nums)):
             if nums[i] == nums[i+1]:
                 return nums[i]
+    '''   
+        
+            
