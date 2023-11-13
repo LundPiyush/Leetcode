c66f6e0893e89e4ead19fd9d2cfc8417f39f19bd
Piyush Lund <55160067+LundPiyush@users.noreply.github.com>
2023-11-14 02:05:20 +0530

    Time: 117 ms (24.27%), Space: 17.9 MB (27.69%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,12 +1,59 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
+        # optimal approach - Extended version of Moore's voting algorithm 
+        n = len(nums)
+        mini_count = n//3
+        cnt1,cnt2=0,0
+        ele1, ele2 = None,None
+        
+        for i in range(n):
+            if cnt1==0 and ele2!= nums[i]:
+                ele1 = nums[i]
+                cnt1+=1
+            elif cnt2==0 and ele1!= nums[i]:
+                ele2 = nums[i]
+                cnt2+=1
+            elif nums[i] == ele1:
+                cnt1+=1
+            elif nums[i] == ele2:
+                cnt2+=1
+            else:
+                cnt1-=1
+                cnt2-=1
+                
+        #Verifying step:
+        ans = []
+        cnt1,cnt2=0,0
+        for i in range(n):
+            if ele1 == nums[i]:
+                cnt1+=1
+            if ele2 == nums[i]:
+                cnt2+=1
+        
+        if cnt1 > mini_count:
+            ans.append(ele1)
+        if cnt2 > mini_count:
+            ans.append(ele2)
+            
+        return ans
+            
+                
+            
+        
+        
+    
+        
+        
+        '''
+        Hashing method TC - > O(nlogn) SC->O(n)
+        
         from collections import Counter
         elements = Counter(nums)
         
         ans =[]
         n = len(nums)
-        
         for key,value in elements.items():
             if value > n //3:
                 ans.append(key)
         return ans
+        '''
