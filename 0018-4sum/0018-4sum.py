00cf516ff5db207198d88b187aad13373a5ef495
Piyush Lund <55160067+LundPiyush@users.noreply.github.com>
2023-11-18 04:05:01 +0530

    Time: 970 ms (29.63%), Space: 16.5 MB (10.59%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -1,5 +1,34 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        ans =[]
+        n = len(nums)
+        nums.sort()
+        for i in range(n):
+            if i>0 and nums[i]== nums[i-1]:
+                continue
+            for j in range(i+1,n):
+                if j!=i+1 and nums[j]== nums[j-1]:
+                    continue
+                k,l = j+1, n-1
+                while k<l:
+                    if nums[i]+ nums[j]+ nums[k]+ nums[l] < target:
+                        k = k+1
+                    elif nums[i]+ nums[j]+ nums[k]+ nums[l] > target:
+                        l = l-1
+                    else:
+                        ans.append([nums[i],nums[j],nums[k],nums[l]])
+                        k = k+1
+                        l = l-1
+                        while k<l and nums[k]==nums[k-1]:
+                            k = k+1
+                        while k<l and nums[l]==nums[l+1]:
+                            l = l-1
+        return ans
+        '''
+        Better approach 
+        TC -> O(N3*log(M)), where N = size of the array, M = no. of elements in the set.
+        SC=> O(2 * no. of the quadruplets)+O(N)
+        
         ans = []
         st = set()
         n = len(nums)
@@ -15,3 +44,4 @@ class Solution:
                         st.add(tuple(temp))
                     hashset.add(nums[k])
         return list(st)
+        '''
