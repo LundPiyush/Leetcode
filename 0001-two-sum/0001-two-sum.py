abf4ea45e2820fe0c7c1c33ca2e9edb8b220d347
Piyush Lund <55160067+LundPiyush@users.noreply.github.com>
2023-11-17 21:53:24 +0530

    Time: 65 ms (65.18%), Space: 17.6 MB (36.45%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -7,3 +7,22 @@ class Solution:
                 return [hashing[more],i]
             hashing[nums[i]] = i
         return {-1,-1}
+    '''
+        two pointer approach  yes or no 
+        nums.sort()
+        n = len(nums)
+        left,right = 0 , n-1
+        # [2,3,4]
+        while left <= right:
+            if nums[left] + nums[right] < target:
+                left+=1
+            elif nums[left] + nums[right] > target:
+                right-=1
+            else:
+                return "YES"
+        return "NO"
+    ''' 
+        
+    
+        
+    
