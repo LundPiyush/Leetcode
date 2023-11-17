23aa1ad352c9287621f1a7030f3c104e9ecdb958
Piyush Lund <55160067+LundPiyush@users.noreply.github.com>
2023-11-18 01:22:12 +0530

    Time: 1324 ms (38.72%), Space: 20.5 MB (43.27%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,5 +1,32 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        ans = []
+        n = len(nums)
+        nums.sort()
+        
+        for i in range(n):
+            if i>0 and nums[i] == nums[i-1]:
+                continue
+            j,k = i+1,n-1
+            while j <k:
+                if nums[i] + nums[j] + nums[k] < 0:
+                    j = j+1
+                elif nums[i] + nums[j] + nums[k] > 0:
+                    k=k-1
+                else:
+                    ans.append([nums[i],nums[j],nums[k]])
+                    j = j+1
+                    k = k-1
+                    while j < k and nums[j] == nums[j-1]:
+                        j = j+1
+                    while j < k and nums[k] == nums[k+1]:
+                        k = k-1
+        return ans 
+    
+    
+    
+    
+        '''
         ans =[]
         st = set()
         n = len(nums)
@@ -14,7 +41,8 @@ class Solution:
                 hashset.add(nums[j])
         ans = list(st)
         return ans
-    
+        '''
+        
         '''
         BRUTE FORCE APPROACH: TC -> O(N^3)+ O(NO. OF UNIQUE TRIPPLETS FOR STORING IN SET) SC->O(2* NO. OF UNIQUE TRIPPLETS)
         
