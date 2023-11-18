c49d9874e15cea783b28e069392da3005b0d10db
Piyush Lund <55160067+LundPiyush@users.noreply.github.com>
2023-11-18 19:50:28 +0530

    Time: 392 ms (63.18%), Space: 31.3 MB (50.12%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -1,6 +1,11 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        
+        ''' Time Complexity: O(N) + O(2*N) ~ O(3*N), where N = size of the array.
+            Reason: O(N) for putting all the elements into the set
+                    After that for every starting element, we are finding the consecutive elements. Though                       we are using nested loops, the set will be traversed at most twice in the worst case. So, the time complexity is O(2*N) instead of O(N2).
+
+Space Complexity: O(N), as we are using the set data structure to solve this problem.
+'''
         n = len(nums)
         st= set()
         if n ==0:
