1876d327c070e453d2efdcc2e0fb56a1423ee49e
Piyush Lund <55160067+LundPiyush@users.noreply.github.com>
2023-11-22 02:00:18 +0530

    Time: 280 ms (9.01%), Space: 20.8 MB (7.70%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -0,0 +1,36 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        mpp = defaultdict(int)
+        preSum = 0
+        cnt = 0
+
+        mpp[0] = 1 # Setting 0 in the map.
+        for i in range(n):
+            # add current element to prefix Sum:
+            preSum += nums[i]
+
+            # Calculate x-k:
+            remove = preSum - k
+
+            # Add the number of subarrays to be removed:
+            cnt += mpp[remove]
+
+            # Update the count of prefix sum in the map.
+            mpp[preSum] += 1
+
+        return cnt
+        
+        '''
+        # Brute force        
+        n = len(nums)
+        count = 0
+        
+        for i in range(n):
+            sum =0
+            for j in range(i,n):
+                sum += nums[j]
+                if sum == k:
+                    count +=1
+        return count
+        '''
