7ae472ae68def4f6e8a95c7ccf3d1f7850e97511
Piyush Lund <55160067+LundPiyush@users.noreply.github.com>
2023-11-23 01:03:08 +0530

    Time: 66 ms (42.09%), Space: 16.3 MB (83.62%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,5 +1,25 @@
 class Solution:
     def lengthOfLongestSubstring(self, a: str) -> int:
+        
+        if len(a) == 0:
+            return 0
+        n = len(a)
+        map = {}
+        maxi = 0
+        left, right =0,0
+        while right < n:
+            if a[right] in map:
+                left = max(map[a[right]] + 1,left)    
+            
+            map[a[right]] = right
+            maxi = max(maxi,right-left+1)
+            right+=1
+        return maxi
+
+    
+    '''
+    # Better solution 
+    TC - O(2N)      SC -> O(N)
         if len(a) == 0:
             return 0
         n = len(a)
@@ -14,7 +34,7 @@ class Solution:
             maxi = max(maxi,right-left+1)
             right+=1
         return maxi
-            
+    '''
         
        
             
