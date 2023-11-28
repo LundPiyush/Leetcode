68d129d69b7c043c07c37ed553a5b56acaacaac3
Piyush Lund <55160067+LundPiyush@users.noreply.github.com>
2023-11-28 23:59:44 +0530

    Time: 47 ms (5.17%), Space: 16.2 MB (73.75%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
@@ -5,23 +5,31 @@
 #         self.next = next
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        '''
+        # Brute force approach 
         
         n = 0 
         temp = head 
+        # counting nodes in LinkedList
         while temp:
             n+=1
             temp= temp.next
+            
         temp = head   
+        # traversing n/2 times to find the middle node
         for i in range(n//2):
             temp = temp.next
             
         return temp   
     '''
+        # Hare-Tortoise approach
+        
         fast, slow = head,head
+        
         while fast and fast.next:
             fast = fast.next.next
             slow = slow.next
         
         return slow
     
-    '''
+    
