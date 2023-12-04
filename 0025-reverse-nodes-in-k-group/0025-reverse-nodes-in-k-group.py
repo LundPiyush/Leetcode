27dd42189da3c778546edd79ad168ff3826c34fc
Piyush Lund <55160067+LundPiyush@users.noreply.github.com>
2023-12-04 18:47:21 +0530

    Time: 55 ms (37.15%), Space: 17.7 MB (11.73%) - LeetHub

---
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.py
@@ -0,0 +1,43 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self,head):
+        cur= head
+        next_p,prev= None,None
+        
+        while cur:
+            next_p= cur.next
+            cur.next = prev
+            prev = cur
+            cur = next_p
+        return prev
+    def getKthNode(self,temp,k):
+        k = k-1
+        while temp!=None and k>0:
+            k = k-1
+            temp = temp.next
+        return temp
+        
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        temp = head
+        prevNode,nextNode = None,None
+
+        while temp!=None:
+            kthNode = self.getKthNode(temp,k)
+            if kthNode == None:
+                if prevNode:
+                    prevNode.next = temp
+                    break
+            nextNode = kthNode.next
+            kthNode.next = None
+            self.reverseList(temp)
+            if temp == head:
+                head = kthNode
+            else:
+                prevNode.next = kthNode
+            prevNode = temp
+            temp = nextNode
+        return head
