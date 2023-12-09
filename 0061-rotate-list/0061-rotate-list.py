d3008fa86c8c5c7d4139e53c42e99db1f0bf197e
Piyush Lund <55160067+LundPiyush@users.noreply.github.com>
2023-12-09 17:08:37 +0530

    Time: 47 ms (30.45%), Space: 16.3 MB (28.74%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.py
+++ 0061-rotate-list/0061-rotate-list.py
@@ -7,18 +7,22 @@ class Solution:
     def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
         if head == None or head.next == None or k == 0:
             return head
-        tail = head
-        n = 1
-        while tail.next !=None:
-            tail = tail.next
-            n = n + 1
-        k = k%n
-        tail.next = head
-        count = n - k
-        ans = head
-        while count>1:
-            count = count-1
-            ans = ans.next
-        head = ans.next
-        ans.next = None
-        return head
+    # calculating length
+        temp = head
+        length = 1
+        while temp.next != None:
+            length += 1
+            temp = temp.next
+    # link last node to first node
+        temp.next = head
+        k = k % length  # when k is more than length of list
+        end = length - k  # to get end of the list
+        while end:
+            temp = temp.next
+            end -= 1
+    # breaking last node link and pointing to NULL
+        head = temp.next
+        temp.next = None
+
+
+        return head 
