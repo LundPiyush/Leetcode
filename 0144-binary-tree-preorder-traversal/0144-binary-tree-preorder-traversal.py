5fa33056b89ced74a0d5179b46017bed85ac20bf
Piyush Lund <55160067+LundPiyush@users.noreply.github.com>
2023-12-15 00:37:14 +0530

    Time: 38 ms (57.37%), Space: 16.3 MB (56.88%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.py
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.py
@@ -7,6 +7,25 @@
 class Solution:
     def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         ans = []
+        stack=[root]
+        
+        while stack:
+            temp = stack.pop()
+            if temp:
+                ans.append(temp.val)
+                stack.append(temp.right) #as we are using stack which works on LIFO, we need to push right tree first so that left will be popped out
+                stack.append(temp.left)
+        return ans
+            
+        
+        
+        
+        
+        
+        
+        '''
+        Recursive approach
+        ans = []
         if not root:
             return ans
         def preOrder(node):
@@ -17,4 +36,4 @@ class Solution:
         preOrder(root)
         return ans
             
-            
+        '''
