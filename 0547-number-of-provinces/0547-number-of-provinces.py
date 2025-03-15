e2fdc267dd3899881608933eb96883c53cc12c59
Piyush Lund <55160067+LundPiyush@users.noreply.github.com>
2025-03-16 00:47:32 +0530

    Time: 8 ms (38.63%), Space: 19.1 MB (57.55%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -6,7 +6,7 @@ class Solution:
                 self.dfs(ele,vis,adj)
 
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        V = len(isConnected)
+        V = len(isConnected) # no of nodes
         adj = [[] for _ in range(V)]
 
         # convert adj matrix to adj list
@@ -14,13 +14,13 @@ class Solution:
             for j in range(V):
                 if isConnected[i][j] == 1 and i!=j:
                     adj[i].append(j)
-        print(adj)
-        vis = [False] * V
+                    adj[j].append(i)
+        vis = [False] * V # marking all nodes in visited array as False 
         count = 0
     
         for i in range(V):
             if not vis[i]:
-                count = count + 1
+                count = count + 1 #count will increase when there is new starting node 
                 self.dfs(i,vis,adj)
         return count
 
